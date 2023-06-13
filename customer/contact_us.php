90d5f7f906bfb56122cc84f0f88b701aa26d911d
KyawKoKoSan <kyawkokosan5@gmail.com>
2023-06-13 13:13:32 +0630

    Our Services Page Added

--- customer/contact_us.php
+++ customer/contact_us.php
@@ -6,8 +6,7 @@ include "template/header.php"; ?>
             <div class="row">
                 <div class="col-12 d-flex justify-content-center align-items-center">
                     <div class="col-9 mt-5 pe-0 pt-5 pb-4 pb-xl-5 pt-xl-4">
-                        <img src="customer_assets/img/Contact Us.png" class="img-fluid" alt="Sneaker Photo"
-                            style="border-radius: 30px;">
+                        <img src="customer_assets/img/Contact Us.png" class="img-fluid" alt="Sneaker Photo" style="border-radius: 30px;">
                     </div>
                 </div>
             </div>
@@ -29,33 +28,28 @@ include "template/header.php"; ?>
                                     <label for="inputName" class="form-label">
                                         <i class="feather-user me-2 text-primary"></i>Name
                                     </label>
-                                    <input name="name" type="text" class="form-control" id="inputName"
-                                        placeholder="Eg:John" required>
+                                    <input name="name" type="text" class="form-control" id="inputName" placeholder="Eg:John" required>
                                 </div>
                                 <div class="col-12 mt-2">
                                     <label for="inputEmail4" class="form-label">
                                         <i class="feather-mail me-2 text-primary"></i>Email
                                     </label>
-                                    <input type="email" name="email" class="form-control" id="inputEmail4"
-                                        placeholder="example@gmail.com" required>
+                                    <input type="email" name="email" class="form-control" id="inputEmail4" placeholder="example@gmail.com" required>
                                 </div>
                                 <div class="col-12 mt-2">
                                     <label for="inputPhone" class="form-label">
                                         <i class="feather-phone me-2 text-primary"></i>Phone Number
                                     </label>
-                                    <input type="number" name="phone" class="form-control" id="inputPhone"
-                                        placeholder="+95912345678" required>
+                                    <input type="number" name="phone" class="form-control" id="inputPhone" placeholder="+95912345678" required>
                                 </div>
                                 <div class="col-12 mt-2">
                                     <div>
                                         <label for="longDes" class="form-label my-3">Message</label>
-                                        <textarea name="description" class="form-control" style="resize: none;"
-                                            id="longDes" rows="5"></textarea>
+                                        <textarea name="description" class="form-control" style="resize: none;" id="longDes" rows="5"></textarea>
                                     </div>
                                 </div>
                                 <div class="col-12 mt-2">
-                                    <button type="submit" name="send"
-                                        class="btn btn-outline-primary col-12 col-lg-3">Send</button>
+                                    <button type="submit" name="send" class="btn btn-outline-primary col-12 col-lg-3">Send</button>
                                 </div>
                             </form>
                         </div>
@@ -63,9 +57,7 @@ include "template/header.php"; ?>
                 </div>
                 <div class="col-12 col-lg-6 mt-3">
                     <div class="col-12 col-lg-10 text-center wow animate__bounceInRight">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d488799.4874372742!2d95.9013768351299!3d16.838952489086928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c1949e223e196b%3A0x56fbd271f8080bb4!2sYangon%2C%20Myanmar%20(Burma)!5e0!3m2!1sen!2ssg!4v1645607641218!5m2!1sen!2ssg"
-                            width="400" height="500" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d488799.4874372742!2d95.9013768351299!3d16.838952489086928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c1949e223e196b%3A0x56fbd271f8080bb4!2sYangon%2C%20Myanmar%20(Burma)!5e0!3m2!1sen!2ssg!4v1645607641218!5m2!1sen!2ssg" width="400" height="500" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                     </div>
                 </div>
             </div>
---
+++ customer/our_services.php
@@ -0,0 +1,171 @@
+<?php include "template/header.php"; ?>
+
+
+<section class="container" id="home">
+    <div class="row py-5 justify-content-center align-items-end">
+        <div class="row mt-5 wow animate__slideInDown">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb text-uppercase">
+                    <li class="breadcrumb-item"><a href="our_services.php" class="text-decoration-none ">Our
+                            Services</a>
+                    </li>
+                </ol>
+            </nav>
+        </div>
+        <div class="row flex-column-reverse flex-lg-row mt-5" id="about">
+            <div class="col-12 col-lg-9">
+                <div class="row wow animate__slideInLeft">
+                    <div class="col-12 col-lg-4 ">
+                        <div class="card mb-5 product-card">
+                            <a href="">
+                                <img src="customer_assets/img/life_insurance.jpg" class="img-fluid card-img-top card-product-img"></img>
+                            </a>
+                            <div class="card-body">
+                                <p class="card-title fw-bold mb-2">Life Insurance</p>
+                                <p class="card-text d-block mb-2">
+                                    At [Insurance Company Name], we understand the importance of protecting what matters
+                                    most to you—your loved ones and their future. That's why we offer comprehensive and
+                                    reliable life insurance solutions designed to provide financial security and peace
+                                    of mind.
+                                </p>
+                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                    <p class="fw-bold mb-0 user-select-none">1000$</p>
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <a href="" class="btn btn-outline-primary col-12">
+                                        <i class="feather-shopping-cart me-2"></i>
+                                        <p class="d-inline">
+                                            Add To Cart
+                                        </p>
+                                    </a>
+                                </div>
+                                <div class="col-12">
+                                    <a href="" class="btn btn-outline-primary col-12">
+                                        <i class="feather-info me-2"></i>
+                                        <p class=" d-inline">View Details</p>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-4 ">
+                        <div class="card mb-5 product-card">
+                            <a href="">
+                                <img src="customer_assets/img/life_insurance.jpg" class="img-fluid card-img-top card-product-img"></img>
+                            </a>
+                            <div class="card-body">
+                                <p class="card-title fw-bold mb-2">Life Insurance</p>
+                                <p class="card-text d-block mb-2">
+                                    At [Insurance Company Name], we understand the importance of protecting what matters
+                                    most to you—your loved ones and their future. That's why we offer comprehensive and
+                                    reliable life insurance solutions designed to provide financial security and peace
+                                    of mind.
+                                </p>
+                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                    <p class="fw-bold mb-0 user-select-none">1000$</p>
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <a href="" class="btn btn-outline-primary col-12">
+                                        <i class="feather-shopping-cart me-2"></i>
+                                        <p class="d-inline">
+                                            Add To Cart
+                                        </p>
+                                    </a>
+                                </div>
+                                <div class="col-12">
+                                    <a href="" class="btn btn-outline-primary col-12">
+                                        <i class="feather-info me-2"></i>
+                                        <p class=" d-inline">View Details</p>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-4 ">
+                        <div class="card mb-5 product-card">
+                            <a href="">
+                                <img src="customer_assets/img/life_insurance.jpg" class="img-fluid card-img-top card-product-img"></img>
+                            </a>
+                            <div class="card-body">
+                                <p class="card-title fw-bold mb-2">Life Insurance</p>
+                                <p class="card-text d-block mb-2">
+                                    At [Insurance Company Name], we understand the importance of protecting what matters
+                                    most to you—your loved ones and their future. That's why we offer comprehensive and
+                                    reliable life insurance solutions designed to provide financial security and peace
+                                    of mind.
+                                </p>
+                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                    <p class="fw-bold mb-0 user-select-none">1000$</p>
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <a href="" class="btn btn-outline-primary col-12">
+                                        <i class="feather-shopping-cart me-2"></i>
+                                        <p class="d-inline">
+                                            Add To Cart
+                                        </p>
+                                    </a>
+                                </div>
+                                <div class="col-12">
+                                    <a href="" class="btn btn-outline-primary col-12">
+                                        <i class="feather-info me-2"></i>
+                                        <p class=" d-inline">View Details</p>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-lg-3 wow animate__bounceInRight delay-5s">
+                <div class="sticky-for-promo">
+                    <div class="col-12">
+                        <form class="d-flex" action="" method="post">
+                            <input name="searchKey" class="form-control me-sm-2" type="text" placeholder="Search">
+                            <button class="btn btn-outline-primary my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div>
+                    <div class="row my-3">
+                        <h3 class="fw-bold">Choose Category</h3>
+                    </div>
+                    <div class="list-group col-12 mb-3">
+                        <a href="" class="list-group-item list-group-item-action">All
+                            Categories</a>
+                        <a href="" class="fw-bold list-group-item list-group-item-action">Life Insurance</a>
+                        <a href="" class=" list-group-item list-group-item-action">Life Insurance</a>
+                        <a href="" class=" list-group-item list-group-item-action">Life Insurance</a>
+                    </div>
+                    <div class="row d-none d-lg-block">
+                        <h3 class="fw-bold mb-3">Best Offers</h3>
+                    </div>
+                    <div class="slickTest row d-none d-lg-block">
+                        <div class="col-12 mx-2">
+                            <div class="card slick-card">
+                                <img src="customer_assets/img/life_insurance.jpg" class="card-img-top card-product-img img-fluid" alt="...">
+                                <div class="card-body">
+                                    <h5>Life Insurance</h5>
+                                    <p style="text-align: justify">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda autem,
+                                        commodi deserunt eum exercitationem fugiat in labore neque non nulla officia
+                                        tempora tempore ut v
+                                    </p>
+                                    <h6><span class="text-decoration-line-through me-2  original-price-text">100$</span><span class="final-price-text">90$</span></h6>
+                                    <div class="row">
+                                        <div class="col-12 d-flex justify-content-center">
+                                            <a href="" class="btn btn-outline-primary col-12 col-md-6">Details</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</section>
+
+
+<?php include "template/footer.php"; ?>
