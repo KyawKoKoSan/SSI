291e2adb2e331aeb545a7c17089654e94132f736
KyawKoKoSan <kyawkokosan5@gmail.com>
2023-06-15 11:03:20 +0630

    Adding Image FIlter functions

--- admin/core/functions.php
+++ admin/core/functions.php
@@ -27,4 +27,56 @@ function checkPassword($password,$original_password=""){
     }
 }
 
+function imageFilter($files,$original_image=""){
+    if ($files['tmp_name']!="") {
+        $file_name = $files['name'];
+        $file_size = $files['size'];
+        $file_tmp = $files['tmp_name'];
+        $file_type = $files['type'];
+        $f_ext = explode("/", $file_type);
+        $file_ex = strtolower(end($f_ext));
+        $extension = array("jpeg", "png", "jpg", "gif");
+        if (in_array($file_ex, $extension) == FALSE) {
+            $errors[] = "Please Upload the valid file type";
+        } else if ($file_size > 2097152) {
+            $errors[] = "File size is too big";
+        } else if (empty($errors) == TRUE) {
+            move_uploaded_file($file_tmp, "images/" . $file_name);
+            return $file_name;
+        }
+        else {
+            print_r($errors);
+        }
+    }
+    else {
+        return $file_name=$original_image;
+    }
+}
+
+function imageFilterRegistration($files,$original_image=""){
+    if ($files['tmp_name']!="") {
+        $file_name = $files['name'];
+        $file_size = $files['size'];
+        $file_tmp = $files['tmp_name'];
+        $file_type = $files['type'];
+        $f_ext = explode("/", $file_type);
+        $file_ex = strtolower(end($f_ext));
+        $extension = array("jpeg", "png", "jpg", "gif");
+        if (in_array($file_ex, $extension) == FALSE) {
+            $errors[] = "Please Upload the valid file type";
+        } else if ($file_size > 2097152) {
+            $errors[] = "File size is too big";
+        } else if (empty($errors) == TRUE) {
+            move_uploaded_file($file_tmp, "images/" . $file_name);
+            return $file_name;
+        }
+        else {
+            print_r($errors);
+        }
+    }
+    else {
+        return $file_name="default.png";
+    }
+}
+
 //common functions start here
