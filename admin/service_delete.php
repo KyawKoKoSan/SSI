de3f4e28f2f2bb2e6af6e2ce981de41f364d9399
KyawKoKoSan <kyawkokosan5@gmail.com>
2023-06-20 08:18:13 +0630

    Service Delete added (Lwin Lwin Moon)

---
+++ admin/service_delete.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once "core/admin_auth.php";
+require_once "core/base.php";
+require_once "core/functions.php" ;
+$id = $_GET['id'];
+
+if(serviceDelete($id)){
+    linkTo('service_list.php');
+}
